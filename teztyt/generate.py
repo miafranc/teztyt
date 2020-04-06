6d5ea15bab2ff834ffa30a443ca7cc29a46187c9
miafranc <miafranc@miafranc-Lenovo-ideapad-520S-14IKB>
2020-04-06 22:26:10 +0300

    Migrate to Python 3.

--- teztyt/generate.py
+++ teztyt/generate.py
@@ -26,7 +26,7 @@ class OneClassMultipleChoiceTest:
         """Constructor of class `OneClassMultipleChoiceTest`.
         
         Parameters:
-            config_file (str or unicode): Path to the JSON configuration file.
+            config_file (str): Path to the JSON configuration file.
         """
         self.config = self.load_json(config_file)
         self.data = []
@@ -36,7 +36,7 @@ class OneClassMultipleChoiceTest:
         """Returns the content of a JSON file.
         
         Parameters:
-            json_filename (str or unicode): Path to the filename.
+            json_filename (str): Path to the filename.
         
         Returns:
             dict: JSON as a dictionary.
@@ -51,7 +51,7 @@ class OneClassMultipleChoiceTest:
         Must be called before any other method.
         
         Parameters:
-            *filenames (strs or unicodes): Paths to the JSON data files.
+            *filenames (strs): Paths to the JSON data files.
         """
         self.data = []
         for f in filenames:
@@ -63,12 +63,12 @@ class OneClassMultipleChoiceTest:
         
         Parameters:
             num_tests (int): Number of tests to generate.
-            out_dir (str or unicode): Path to output directory.
+            out_dir (str): Path to output directory.
             *num_problems (ints): Number of problems to generate from each data file.
         """
         map(lambda x: shutil.rmtree(x) if isdir(x) else unlink(x), (join(out_dir, f) for f in listdir(out_dir)))
 
-        solutions = u'===\n'
+        solutions = '===\n'
         
         for test_id in range(num_tests):
             (code, sol) = self.generate_test(test_id + 1, *num_problems)
@@ -90,7 +90,7 @@ class OneClassMultipleChoiceTest:
             if page_num > self.config['max_pages']:
                 raise Exception('Could not generate tests: could not fit into given number of pages!')
             
-            solutions += sol + u'===\n'
+            solutions += sol + '===\n'
             
         f = codecs.open(join(out_dir, self.config['solutions_file']), 'w', 'utf-8')
         f.write(solutions)
@@ -105,19 +105,18 @@ class OneClassMultipleChoiceTest:
             *num_problems (ints): Number of problems to generate from each data file.
         
         Returns:
-            unicode: LaTeX code for one test.
-            unicode: Solution of the test.
+            str: LaTeX code for one test.
+            str: Solution of the test.
         """
         if len(num_problems) != len(self.data):
             raise Exception('Number of data files and number of problems must be equal!')
     
         all_keys = [self.data[i].keys() for i in range(len(self.data))]
-#         print all_keys
         selected_keys = []
         problem_num = 0
         
-        test_code = u''
-        test_solution = u''
+        test_code = ''
+        test_solution = ''
         
         for i in range(len(num_problems)):
             if num_problems[i] > len(all_keys[i]):
@@ -134,15 +133,8 @@ class OneClassMultipleChoiceTest:
                 test_code += code
                 test_solution += sol
             
-#         print selected_keys
-#         print test_code
-#         print test_solution
-        
         test_code = self._generate_code_prologue(test_id) + test_code + self._generate_code_epilogue()
-        test_solution = unicode(test_id) + u':\n' + test_solution
-        
-#         print test_code
-#         print test_solution
+        test_solution = str(test_id) + ':\n' + test_solution
         
         return (test_code, test_solution)
 
@@ -152,19 +144,19 @@ class OneClassMultipleChoiceTest:
         Parameters:
             test_id (int): Unique ID (usually index) of test.
             problems (list): Exact problems to generate from each data file (IDs).
-            out_dir (str or unicode): Path to output directory.
+            out_dir (str): Path to output directory.
         
         Returns:
-            unicode: LaTeX code for one test.
-            unicode: Solution of the test.
+            str: LaTeX code for one test.
+            str: Solution of the test.
         """
         if len(problems) != len(self.data):
             raise Exception('Number of data files and number of problems must be equal!')
     
         problem_num = 0
         
-        test_code = u''
-        test_solution = u''
+        test_code = ''
+        test_solution = ''
         
         for i in range(len(problems)):
             for k in problems[i]:
@@ -175,15 +167,8 @@ class OneClassMultipleChoiceTest:
                 test_code += code
                 test_solution += sol
             
-#         print selected_keys
-#         print test_code
-#         print test_solution
-        
         test_code = self._generate_code_prologue(test_id) + test_code + self._generate_code_epilogue()
-        test_solution = unicode(test_id) + u':\n' + test_solution
-        
-#         print test_code
-#         print test_solution
+        test_solution = str(test_id) + ':\n' + test_solution
         
         self._write_latex(test_id, test_code, out_dir)
         self._compile_latex(test_id, out_dir)
@@ -198,29 +183,27 @@ class OneClassMultipleChoiceTest:
             test_id (int): Unique ID (usually index) of test. 
             problem_num (int): Index of problem.
             i (int): Index of data file.
-            k (str or unicode): Key of the problem.
-            answers (list of strs/unicodes): Shuffled keys of the problem.
+            k (str): Key of the problem.
+            answers (list of strs): Shuffled keys of the problem.
             
         Returns:
-            unicode: LaTeX code of a problem.
+            str: LaTeX code of a problem.
         """
-        code = u'%{}:\n'.format(problem_num)
-        code += u'\\begin{{{}}}[{}p]%{}/{}\n'.format(self.config['problem_environment'], self.data[i][k]['P'], i, k)
+        code = '%{}:\n'.format(problem_num)
+        code += '\\begin{{{}}}[{}p]%{}/{}\n'.format(self.config['problem_environment'], self.data[i][k]['P'], i, k)
         code += self.data[i][k]['Q'].replace('%figures_dir%', self.config['figures_dir']) + '\n'
-        code += u'\\begin{itemize}\n'
-        code += u'\\setlength{{\\itemsep}}{{{}}}\n'.format(self.config['itemsep'])
+        code += '\\begin{itemize}\n'
+        code += '\\setlength{{\\itemsep}}{{{}}}\n'.format(self.config['itemsep'])
         
         for ind_a, a in enumerate(answers):
             ans = self.data[i][k]['A'][a].replace('%figures_dir%', self.config['figures_dir'])
-#             code += u'\\item[{}] {}'.format(self.config['checkbox'], ans)
-            code += u'\\item[\\checkBoxHref{{{}}}] {}'.format(u'{}:{}:{}:{}'.format(test_id, i, k, ind_a), ans)
-            code += u' %\n' if regex.match(self.config['correct_key_match'], a) else u'\n'
+#             code += '\\item[{}] {}'.format(self.config['checkbox'], ans)
+            code += '\\item[\\checkBoxHref{{{}}}] {}'.format('{}:{}:{}:{}'.format(test_id, i, k, ind_a), ans)
+            code += ' %\n' if regex.match(self.config['correct_key_match'], a) else '\n'
          
-        code += u'\\end{itemize}\n'
-        code += u'\\end{{{}}}\n'.format(self.config['problem_environment'])
+        code += '\\end{itemize}\n'
+        code += '\\end{{{}}}\n'.format(self.config['problem_environment'])
         
-#         print code
-    
         return code
 
     def _generate_code_prologue(self, test_id):
@@ -230,49 +213,47 @@ class OneClassMultipleChoiceTest:
             test_id (int): Unique ID (usually index) of test.
             
         Returns:
-            unicode: The LaTeX code prologue.
+            str: The LaTeX code prologue.
         """
-        prologue = u'\\documentclass[{}pt,oneside,twocolumn]{{extarticle}}\n'.format(self.config['fontsize'])
-        prologue += u'\n'.join(self.config['prologue']) + u'\n\n'
+        prologue = '\\documentclass[{}pt,oneside,twocolumn]{{extarticle}}\n'.format(self.config['fontsize'])
+        prologue += '\n'.join(self.config['prologue']) + '\n\n'
         
-        prologue += u'\\title{{{}\\\\\n'.format(self.config['title'])
+        prologue += '\\title{{{}\\\\\n'.format(self.config['title'])
         if self.config['subtitle'] != '':
-            prologue += u'{}\\\\\n'.format(self.config['subtitle'])
-        prologue += u'{}\\\\\n'.format(test_id)
-        prologue += u'}'
+            prologue += '{}\\\\\n'.format(self.config['subtitle'])
+        prologue += '{}\\\\\n'.format(test_id)
+        prologue += '}'
         
-        prologue += u'\n\\date{}\\author{}\n\n'
+        prologue += '\n\\date{}\\author{}\n\n'
         
-        prologue += u'\\usepackage{hyperref}\n'
-        prologue += u'\\newcommand\\checkBoxHref[1]{\\mbox{\\CheckBox[width=3mm, height=3mm, checkboxsymbol=\\ding{110}, bordercolor=0 0 0]{#1}}}\n'
-        prologue += u'\\renewcommand\\LayoutCheckField[2]{#2}\n\n'
+        prologue += '\\usepackage{hyperref}\n'
+        prologue += '\\newcommand\\checkBoxHref[1]{\\mbox{\\CheckBox[width=3mm, height=3mm, checkboxsymbol=\\ding{110}, bordercolor=0 0 0]{#1}}}\n'
+        prologue += '\\renewcommand\\LayoutCheckField[2]{#2}\n\n'
 
-        prologue += u'\\pagenumbering{{{}}}\n'.format(self.config['pagenumbering'])
-        prologue += u'\\newcounter{{fel}}\n\\newtheorem{{problem}}[fel]{{{}}}\n'.format(self.config['newtheorem_string'])  # "built-in"/default problem environment
-        prologue += u'\\renewcommand{{\\baselinestretch}}{{{}}}\n\n'.format(self.config['baselinestretch'])
-        prologue += u'\\begin{document}\n\n\\maketitle\\sloppy\n\n'
+        prologue += '\\pagenumbering{{{}}}\n'.format(self.config['pagenumbering'])
+        prologue += '\\newcounter{{fel}}\n\\newtheorem{{problem}}[fel]{{{}}}\n'.format(self.config['newtheorem_string'])  # "built-in"/default problem environment
+        prologue += '\\renewcommand{{\\baselinestretch}}{{{}}}\n\n'.format(self.config['baselinestretch'])
+        prologue += '\\begin{document}\n\n\\maketitle\\sloppy\n\n'
         
-#         prologue += u'\\begin{Form}[action={}]\n\n'
-        prologue += u'\\begin{Form}\n\n'
+#         prologue += '\\begin{Form}[action={}]\n\n'
+        prologue += '\\begin{Form}\n\n'
 
         for ind_f, field in enumerate(self.config['name_and_stuff']):
-            prologue += u'\\noindent\\TextField[name={},width={}]{{{}:}}{}\n'.format(u't{}:{}'.format(test_id, ind_f), 
+            prologue += '\\noindent\\TextField[name={},width={}]{{{}:}}{}\n'.format('t{}:{}'.format(test_id, ind_f), 
                                                                                      self.config['name_and_stuff_widths'][ind_f], 
                                                                                      field,
-                                                                                     u'\\\\\\\\' if ind_f < len(self.config['name_and_stuff']) -1 else u'')
+                                                                                     '\\\\\\\\' if ind_f < len(self.config['name_and_stuff']) -1 else '')
         
-#         print prologue
-    
         return prologue
     
     def _generate_code_epilogue(self):
         """Generates LaTeX code epilogue.
         
         Returns:
-            unicode: The LaTeX code epilogue.
+            str: The LaTeX code epilogue.
         """
-        epilogue = u'\n\\end{Form}\n\n'
-        epilogue += u'\n\\end{document}'
+        epilogue = '\n\\end{Form}\n\n'
+        epilogue += '\n\\end{document}'
         return epilogue
     
     def _generate_solution(self, problem_num, i, k, answers):
@@ -281,26 +262,24 @@ class OneClassMultipleChoiceTest:
         Parameters:
             problem_num (int): Index of problem.
             i (int): Index of data file.
-            k (str or unicode): Key of the problem.
-            answers (list of strs/unicodes): Shuffled keys of the problem.
+            k (str): Key of the problem.
+            answers (list of strs): Shuffled keys of the problem.
             
         Returns:
-            unicode: Text describing the solution of a given problem.
+            str: Text describing the solution of a given problem.
         """
-        sol = u'{} ({}/{}): '.format(problem_num, i + 1, k)
+        sol = '{} ({}/{}): '.format(problem_num, i + 1, k)
         first_correct_answer = True
         for ind, a in enumerate(answers):
             if first_correct_answer:
                 if regex.match(self.config['correct_key_match'], a):
-                    sol += u'{}'.format(ind + 1)
+                    sol += '{}'.format(ind + 1)
                     first_correct_answer = False
             else:
                 if regex.match(self.config['correct_key_match'], a):
-                    sol += u', {}'.format(ind + 1)
+                    sol += ', {}'.format(ind + 1)
          
-#         print sol
-         
-        return sol + u'\n'
+        return sol + '\n'
     
     def _shuffle_answers(self, problem):
         """Shuffles the answers of a given problem.
@@ -309,10 +288,9 @@ class OneClassMultipleChoiceTest:
             problem (dict): Problem as a dictionary.
             
         Returns:
-            list of strs/unicodes: Shuffled keys of the problem.
+            list of strs: Shuffled keys of the problem.
         """
         keys = random.sample(problem['A'].keys(), len(problem['A']))
-#         print keys
         return keys
 
     def _write_latex(self, test_id, test_code, out_dir):
@@ -320,8 +298,8 @@ class OneClassMultipleChoiceTest:
         
         Parameters:
             test_id (int): Unique ID (usually index) of test.
-            test_code (str or unicode): Generated LaTeX code of test.
-            out_dir (str or unicode): Path to output directory.
+            test_code (str): Generated LaTeX code of test.
+            out_dir (str): Path to output directory.
         """
         fname = '{}{}.tex'.format(self.config['out_file_prefix'], test_id)
         f = codecs.open(join(out_dir, fname), 'w', 'utf-8')
@@ -333,7 +311,7 @@ class OneClassMultipleChoiceTest:
         
         Parameters:
             test_id (int): Unique ID (usually index) of test.
-            out_dir (str or unicode): Path to output directory.
+            out_dir (str): Path to output directory.
         """
         fname = '{}{}.tex'.format(self.config['out_file_prefix'], test_id)
         
@@ -350,12 +328,12 @@ class OneClassMultipleChoiceTest:
         
         Parameters:
             test_id (int): Unique ID (usually index) of test.
-            out_dir (str or unicode): Path to output directory.
+            out_dir (str): Path to output directory.
             
         Returns:
             int: Number of pages in PDF.
         """
-        f = codecs.open(join(out_dir, '{}{}.log'.format(self.config['out_file_prefix'], test_id)))
+        f = codecs.open(join(out_dir, '{}{}.log'.format(self.config['out_file_prefix'], test_id)), 'r', 'utf-8', errors='ignore')  # sometimes utf-8 decode error occurs; did not happen in Python 2.7
         data = f.read()
         f.close()
         
@@ -370,8 +348,8 @@ class OneClassMultipleChoiceTest:
         will have `max_pages` number of pages.
         
         Parameters:
-            in_dir (str or unicode): Path to the input directory containing the PDF files to be merged.
-            out_file (str or unicode): Path to the merged PDF.
+            in_dir (str): Path to the input directory containing the PDF files to be merged.
+            out_file (str): Path to the merged PDF.
         """
         pw = PdfFileWriter()
         
@@ -417,7 +395,7 @@ if __name__ == "__main__1":
 #     main("-c ./config.json -n 1 -f ./data_OK/t1.json ./data_OK/t2.json ./data_OK/t3.json -p 1 1 1 -o ./ooo".split())
 
 
-if __name__ == "__main__":
+if __name__ == "__main__2":
     mct = OneClassMultipleChoiceTest('config.json')
     
     mct.read('data_OK/t1.json',
@@ -426,16 +404,14 @@ if __name__ == "__main__":
       
     test_id = 100
     mct.generate_test_with_problems(test_id, [['2'], ['2'], ['2']], './ooo')
-#     print test_code
-#     print test_solution
+
 #     mct._write_latex(test_id, test_code, './ooo')
 #     mct._compile_latex(test_id, './ooo')
     
 
-if __name__ == "__main__1":
+if __name__ == "__main__":
     # TODO:
     # ----------
-    # - requirements.txt
     # - README.md
     
     mct = OneClassMultipleChoiceTest('config.json')
@@ -445,7 +421,7 @@ if __name__ == "__main__1":
              'data_OK/t3.json')
       
 #     mct.generate_tests(1, 'gen', 1, 1, 1)
-    mct.generate_tests(1, 'gen', 2, 4, 2)
+    mct.generate_tests(5, 'gen', 2, 4, 2)
 #     mct.generate_tests(1, 'gen', 40, 49, 46)
   
     mct._merge_pdfs('gen', 'gen/all.pdf')
